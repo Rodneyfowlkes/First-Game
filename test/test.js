6fdb874f708cb9bfcc889fdaf90d0f330bb9aad4
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-07-14 17:50:45 -0400

    Adding a file for my stage class and did some basic testing. I also started messing around with some keyboard mapping in the main.js file

--- test/test.js
+++ test/test.js
@@ -2,20 +2,44 @@
 import chai from 'chai';
 
 // Import Any Files to Test
-
+import {Stage} from "../src/js/stage.js"
 // Set Chai Constants
 const expect = chai.expect;
 const should = chai.should();
 const assert = chai.assert;
 
-describe('Something We Want To Test', function () {
+describe('should have a stage', function () {
 
-  describe('Testing the Creation of Something', function () {
+  describe('should have a class names stage', function () {
 
-    it('should exist after we create it', function () {
-      let x = new Object;
-      expect(x).to.be.an.instanceof(Object);
+    it('it should have an instance of stage', function () {
+      let x = new Stage;
+      expect(x).to.be.an.instanceof(Stage);
     });
+
+    it('It should contain an array of imgs', function() {
+       let x2 = new Stage;
+       expect(x2.grid_imgs).to.be.an("array");
+       
+
+    });
+
+    
+    it('Stage should contain a method that generates a board', function() {
+       let x2 = new Stage;
+       expect(x2.loadboard()).to.be.an("array");
+       
+
+    });
+
+      it('Board method should contain produce string urls', function() {
+       let x2 = new Stage;
+       x2.loadboard();
+       expect(x2.grid_imgs[0]).to.be.a('string');
+       
+
+    });
+
 
   });
 
